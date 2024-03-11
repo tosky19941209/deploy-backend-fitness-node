613e7e49c035ce12332f7f70929c841a8cc1b357
felixeliass1994 <felixeliass1994@gmail.com>
2024-03-12 05:24:51 +1000

    werwerwerweerwer

--- router/router.js
+++ router/router.js
@@ -7,7 +7,7 @@ const { rmSync } = require('fs')
 
 
 router.get('/test', (req, res) => {
-    res.send("Welcome to fitness site")
+    res.send("Welcome to dashboard")
 })
 
 router.get('/video_load', (req, res) => {
