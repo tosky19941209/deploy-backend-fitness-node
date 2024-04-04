c0c5387e07e31919fe3bcad34568e160b375b5b9
felixeliass1994 <felixeliass1994@gmail.com>
2024-04-05 03:15:18 +0900

    wewerwerwer

--- config/env/config.js
+++ config/env/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     port:8081,
-    // mongodb:'mongodb://127.0.0.1:27017/fitness-database',
-    mongodb:'mongodb+srv://jinxiao19941209:rNfoWdcmIlnfvddR@cluster0.j8ekrte.mongodb.net/',
+    mongodb:'mongodb://127.0.0.1:27017/fitness-database',
+    // mongodb:'mongodb+srv://jinxiao19941209:rNfoWdcmIlnfvddR@cluster0.j8ekrte.mongodb.net/',
     index_landmark : {
         nose: 0,
         left_eye_inner: 1,
